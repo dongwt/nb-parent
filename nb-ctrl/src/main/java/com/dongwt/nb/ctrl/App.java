d1dfb89e9a54aaa4b94d444f1879a6156713c321
dongwt <834575475@qq.com>
2016-03-04 10:27:31 +0800

    init

---
+++ nb-ctrl/src/main/java/com/dongwt/nb/ctrl/App.java
@@ -0,0 +1,13 @@
+package com.dongwt.nb.ctrl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ nb-framework/src/main/java/com/dongwt/nb/framework/App.java
@@ -0,0 +1,13 @@
+package com.dongwt.nb.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

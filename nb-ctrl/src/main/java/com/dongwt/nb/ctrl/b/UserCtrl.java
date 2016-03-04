905a8fbefeec858559cd1fe975acd1761c532954
dongwt <834575475@qq.com>
2016-03-04 17:28:44 +0800

    add logback

--- nb-ctrl/src/main/java/com/dongwt/nb/ctrl/b/UserCtrl.java
+++ nb-ctrl/src/main/java/com/dongwt/nb/ctrl/b/UserCtrl.java
@@ -1,5 +1,7 @@
 package com.dongwt.nb.ctrl.b;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +13,13 @@ import com.dongwt.nb.framework.Result;
 @Controller
 @RequestMapping("/user")
 public class UserCtrl extends BaseCtrl {
+	
+	 private static Logger log = LoggerFactory.getLogger(UserCtrl.class);  
 
 	@RequestMapping(value="/login",method={RequestMethod.GET})
 	@ResponseBody
 	public Result login() {
+		log.debug("begin login");
 		Result result = new Result();
 		result.put("msg", "success");
 		return result;
